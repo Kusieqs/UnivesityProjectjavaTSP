026514f1ead996655259a0988be390abb880d28c
Mikolaj Golanski <mikolajgolanski16@gmail.com>
2024-01-12 16:30:50 +0100

    Adding Main class

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int numberOfVertices = 10; // liczba wierzchołków
+        List<Vertex> vertices = randVertices(numberOfVertices); // lista losowo wygenerowanych wierzchołków
+
+        System.out.println("Początkowe wierzchołki:");
+        for (Vertex v : vertices) {
+            System.out.println("Wierzchołek (" + v.getX() + ", " + v.getY() + ")"); //lista poczatkowych wierzcholkow
+        }
+
+        List<Vertex> P = Algorithm.TSP(vertices); //wywolanie metody TSP z klasy Algorithm, ktora zwraca optymalna droge
+
+        System.out.println("\nOptymalna droga:");
+        for (Vertex vertex : P) {
+            System.out.println("Wierzchołek (" + vertex.getX() + ", " + vertex.getY() + ")"); //lista optymalnej drogi
         }
     }
+    private static List<Vertex> randVertices(int numberOfVertices) { //metoda tworzenia listy losowych wierzchołków
+        List<Vertex> vertices = new ArrayList<>(); //lista wierzchołków
+        Random random = new Random(); //inicjalizacja obiektu klasy Random
+
+        for (int i = 0; i < numberOfVertices; i++) { //dodawanie losowych wierzchołków do listy
+            double x = random.nextDouble() * 10; // losowe x od 0 do 10
+            double y = random.nextDouble() * 10; // losowe y od 0 do 10
+            vertices.add(new Vertex(x, y));
+        }
+
+        return vertices; //zwrócenie listy wierzchołków
+    }
+
 }
